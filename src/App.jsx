a41e3454f24a7f58949af705c73493ec422f85a9
Enthusiastic <belaybirhanu407@gmail.com>
2024-01-02 17:20:17 +0300

    all glb have been rendered to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import Pants from "./models/pants/Pant";
+import Shoe from "./models/shoe/Shoe";
+import Shorts from "./models/shorts/Shorts";
 
 export class App extends Component {
   render() {
     return (
       <> 
+      <div>
         <Pants/> 
+      </div>
+      <div>
+        <Shoe/> 
+      </div>
+      <div>
+        <Shorts/> 
+      </div> 
       </>
     );
   }
