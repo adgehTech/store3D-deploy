d5da22c3d92c902314666d2612309e52620eba69
SW_GC LAB <belaybirhanu407@gmail.com>
2024-01-02 23:47:13 +0300

    responsivenes update

--- src/models/shorts/Model.jsx
+++ src/models/shorts/Model.jsx
@@ -14,7 +14,7 @@ export function Model(props) {
     model.scene.rotation.y = elapsedTime * rotationSpeed;
   });
 
-  return <mesh position={[1, -2, 5]}>
+  return <mesh position={[1, -2, 6]}>
               <primitive object={model.scene}/>
           </mesh>
 }
