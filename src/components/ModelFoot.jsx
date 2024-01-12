a6f6f8c1432479cc45140f8bc1cdff7a494a70e1
Adgeh <belaybirhanu407@gmail.com>
2024-01-12 15:23:59 +0300

    model foot responsive

--- src/components/ModelFoot.jsx
+++ src/components/ModelFoot.jsx
@@ -3,20 +3,31 @@ import React from "react";
 
 export default function ModelFoot() {
   return (
-    <Box>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+    <Box sx={{ ml: { sm: 5 } }}>
+      <Box
+        sx={{ display: "flex", justifyContent: { xs: "center", sm: "left" } }}
+      >
         <Typography>SPLY</Typography>
       </Box>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box
+        sx={{ display: "flex", justifyContent: { xs: "center", sm: "left" } }}
+      >
         <Typography>Regular price €92,95</Typography>
       </Box>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box
+        sx={{ display: "flex", justifyContent: { xs: "center", sm: "left" } }}
+      >
         <Typography>DELIVERY WITHIN 4 WEEKS</Typography>
       </Box>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <Typography>SIZE 1 </Typography><Typography color="darkgray"> 2 3 SIZE GUIDE</Typography>
+      <Box
+        sx={{ display: "flex", justifyContent: { xs: "center", sm: "left" } }}
+      >
+        <Typography>SIZE 1 </Typography>
+        <Typography color="darkgray"> 2 3 SIZE GUIDE</Typography>
       </Box>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box
+        sx={{ display: "flex", justifyContent: { xs: "center", sm: "left" } }}
+      >
         <Typography>ORDER </Typography>
       </Box>
     </Box>
--- src/models/Models.jsx
+++ src/models/Models.jsx
@@ -1,28 +1,19 @@
 import React from "react";
 import Pants from "./pants/Pant";
-import { Box, Grid } from "@mui/material";
+import { Box } from "@mui/material";
 import ModelFoot from "../components/ModelFoot";
-import ModelSide from "../components/ModelSide";
 
 export default function Model() {
   return (
-    <Box sx={{ backgroundColor: "gainsboro", height: "100vh" }}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={4} 
-          flex={1}
-          sx={{
-            display: { lg: "block", sm: "none", xs: "none" },
-          }}
+    <>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Box
+          sx={{ backgroundColor: "gainsboro", height: "73vh", width: "50%" }}
         >
-          <ModelSide />
-        </Grid>
-        <Grid  item xs={12} sm={8} >
           <Pants />
-        </Grid>
-      </Grid>
-      <Box flex={1} sx={{ display: { lg: "none", sm: "block" } }}>
-        <ModelFoot />
+        </Box>
       </Box>
-    </Box>
+      <ModelFoot sx={{height: '20vh'}}/>
+    </>
   );
 }
